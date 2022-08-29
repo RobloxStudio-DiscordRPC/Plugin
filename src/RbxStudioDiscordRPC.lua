2eab871f38bce797107e4cc030fb12ba66d91c72
MatusGuy <mateu.axe@gmail.com>
2022-08-29 22:48:54 +0100

    shows if building or doing gui
    improved request function

--- src/RbxStudioDiscordRPC.lua
+++ src/RbxStudioDiscordRPC.lua
@@ -6,42 +6,83 @@ local HttpService = game:GetService("HttpService")
 local StudioService = game:GetService("StudioService")
 local MarketplaceService = game:GetService("MarketplaceService")
 local place = MarketplaceService:GetProductInfo(game.PlaceId)
+local Selection = game:GetService("Selection")
 
-local function request()
-	local curr = StudioService.ActiveScript
-	local response = HttpService:RequestAsync(
-		{
-			Url = "http://localhost:8000/rbxstudioDiscRPC",
-			Method = "POST",
-			Headers = {
-				["Content-Type"] = "application/json"
-			},
-			Body = HttpService:JSONEncode({
-				PROJECT  = place.Name,
-				EDITING  = if curr then {
-					NAME = curr.Name,
-					TYPE = curr.ClassName,
-				} else {},
-			})
-		}
-	)
- 
-	-- Inspect the response table
-	if response.Success then
-		print("Status code:", response.StatusCode, response.StatusMessage)
-		print("Response body:\n", response.Body)
-	else
-		print("The request failed:", response.StatusCode, response.StatusMessage)
-	end
-end
+type EditionType = "SCRIPT"|"GUI"|"BUILD"
+type ScriptClassnames = "Script"|"LocalScript"|"ModuleScript"
 
-local function onClick()
-	local success, message = pcall(request)
-    if not success then
+type RequestBody = {
+	PROJECT: string|nil,
+	EDITING: {
+		NAME: string,
+		TYPE: EditionType,
+		CLASS: ScriptClassnames|nil
+	}|{}
+}
+
+local function request(body: RequestBody)
+	if not body.PROJECT then body.PROJECT = place.Name end
+
+	local success, message = pcall(function()
+		local response = HttpService:RequestAsync(
+			{
+				Url = "http://localhost:8000/rbxstudioDiscRPC",
+				Method = "POST",
+				Headers = {
+					["Content-Type"] = "application/json"
+				},
+				Body = HttpService:JSONEncode(body)
+			}
+		)
+
+		-- Inspect the response table
+		if response.Success then
+			print("Status code:", response.StatusCode, response.StatusMessage)
+			print("Response body:\n", response.Body)
+		else
+			print("The request failed:", response.StatusCode, response.StatusMessage)
+		end
+	end)
+
+	if not success then
         print("Http Request failed:", message)
     end
 end
 
-onClick()
+local function onClick()
+	local curr = StudioService.ActiveScript
+	request({
+		EDITING   = if curr then {
+			NAME  = curr.Name,
+			TYPE  = "SCRIPT",
+			CLASS = curr.ClassName
+		} else {},
+	})
+end
+
+--onClick()
 button.Click:Connect(onClick)
 StudioService:GetPropertyChangedSignal("ActiveScript"):Connect(onClick)
+
+local function somethingSelected()
+	local selected = Selection:Get()
+	if #selected == 0 then return end
+
+	local recentlySelected = selected[#selected]
+	local editionType: EditionType
+
+	if recentlySelected:IsA("GuiObject") then
+		editionType = "GUI"
+	elseif recentlySelected:IsA("PVInstance") then
+		editionType = "BUILD"
+	else return request({EDITING = {}}) end
+
+	request({
+		EDITING = {
+			NAME = recentlySelected.Name,
+			TYPE = editionType
+		}
+	})
+end
+
+Selection.SelectionChanged:Connect(somethingSelected)
